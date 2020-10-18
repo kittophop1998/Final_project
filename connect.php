40281794b33620331a2a8f76adabbdd1c4670884
kittiphop1998 <kittiphopsompuech2@gmail.com>
2020-10-18 22:07:24 +0700

    update1

--- connect.php
+++ connect.php
@@ -9,11 +9,6 @@
     $db = "check_system";
     $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
-	// if (mysqli_connect_errno())
-	// {
-	// 	echo "Database Connect Failed : " . mysqli_connect_error();
-	// 	exit();
-	// }
 
 	$intRejectTime = 1; // Minute
 	$sql = "UPDATE member SET Loginstatus = '0', Lastupdate = '0000-00-00 00:00:00'  WHERE Loginstatus = '1' AND DATE_ADD(Lastupdate, INTERVAL $intRejectTime MINUTE) <= NOW() ";
